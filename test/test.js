b13488ac69a87b9eaa27c2cb01e68b4ea5b213cb
Vladimir Rodkin <mail@vovanr.com>
2018-07-19 11:15:27 +0300

    test(package): Fix `MultiselectTwoSides` import path
    fix #23

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import C from '../';
+import C from '../src';
 
 test('render component block', t => {
 	const wrapper = shallow(<C/>);
